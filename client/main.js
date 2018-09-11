8c485b51d2be4650f1332389c434a4545282b00d
Julia Astakhova <jastakhova@griddynamics.com>
2018-09-10 19:44:41 -0700

    Adding datetimepicker

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import moment from 'moment';
 import todosList from '../imports/components/todosList/todosList';
 import '../imports/startup/accounts-config.js';
 
@@ -11,6 +12,35 @@ angular.module('consensual', [
 
 function onReady() {
   angular.bootstrap(document, ['consensual']);
+
+  var defaults = {
+  		calendarWeeks: true,
+  		showClear: true,
+  		showClose: true,
+  		allowInputToggle: true,
+  		useCurrent: false,
+  		ignoreReadonly: true,
+  		minDate: new Date(),
+  		toolbarPlacement: 'top',
+  		locale: 'en-ca',
+  		icons: {
+  			time: 'fa fa-clock-o',
+  			date: 'fa fa-calendar',
+  			up: 'fa fa-angle-up',
+  			down: 'fa fa-angle-down',
+  			previous: 'fa fa-angle-left',
+  			next: 'fa fa-angle-right',
+  			today: 'fa fa-dot-circle-o',
+  			clear: 'fa fa-trash',
+  			close: 'fa fa-times'
+  		}
+  	};
+
+  	var optionsDatetime = $.extend({}, defaults, {format:'DD-MM-YYYY HH:mm'});
+		var optionsDate = $.extend({}, defaults, {format:'DD-MM-YYYY'});
+		var optionsTime = $.extend({}, defaults, {format:'HH:mm'});
+
+		$('.datetimepicker').datetimepicker(optionsDatetime);
 }
 
 if (Meteor.isCordova) {
