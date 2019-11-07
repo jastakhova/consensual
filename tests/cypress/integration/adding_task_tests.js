96c2f16ebe11e13856eb70621249654471c74ae6
Yulia <ju.astakhova@gmail.com>
2019-11-06 17:23:34 -0800

    More task adding tests with other people

--- tests/cypress/integration/adding_task_tests.js
+++ tests/cypress/integration/adding_task_tests.js
@@ -23,11 +23,64 @@ describe('adding_task', () => {
     it('should be able to add a self task', () => {
         cy.get("input.hideOnTask").click();
 
-        const taskName = "ST " + name1;
+        const taskName = "ST" + Math.floor(Math.random() * 100) + name1;
         cy.get("textarea[name='newTask']").type(taskName);
 
         cy.get("span.circle").click();
         cy.get("input[name='newReceiver']").should('have.value', name1);
+
+        cy.get("button.pull-right").should('have.attr', 'disabled', 'disabled');
+        cy.get("div.tomorrow").should('contain', 'tomorrow');
+        cy.get("div.tomorrow").click();
+        cy.get("button.pull-right").click();
+        cy.wait(300);
+        cy.get("a.taskLink").contains(taskName.substr(0, 10)).should('exist');
+    });
+
+    it('should be able to add task with someone else by choosing from popular', () => {
+        cy.visit('https://dev.consensu.al');
+        cy.get("a.at-signin").click();
+
+        cy.get('input[name="at-field-email"]').type(randomEmail1);
+        cy.get('input[name="at-field-password"]').type(password1);
+        cy.get('button[type=submit]').click();
+
+        cy.url().should('eq', 'https://dev.consensu.al/#!/tab/todo');
+
+        cy.get("input.hideOnTask").click();
+
+        const taskName = "PT" + Math.floor(Math.random() * 100) + name1;
+        cy.get("textarea[name='newTask']").type(taskName);
+
+        const receiver = 'All Consensual';
+        cy.get("img[title='" + receiver + "']").click();
+        cy.get("input[name='newReceiver']").should('have.value', receiver);
+
+        cy.get("button.pull-right").should('have.attr', 'disabled', 'disabled');
+        cy.get("div.tomorrow").should('contain', 'tomorrow');
+        cy.get("div.tomorrow").click();
+        cy.get("button.pull-right").click();
+        cy.wait(300);
+        cy.get("a.taskLink").contains(taskName.substr(0, 10)).should('exist');
+    });
+
+    it('should be able to add task with someone else by typing', () => {
+        cy.visit('https://dev.consensu.al');
+        cy.get("a.at-signin").click();
+
+        cy.get('input[name="at-field-email"]').type(randomEmail1);
+        cy.get('input[name="at-field-password"]').type(password1);
+        cy.get('button[type=submit]').click();
+
+        cy.url().should('eq', 'https://dev.consensu.al/#!/tab/todo');
+
+        cy.get("input.hideOnTask").click();
+
+        const taskName = "TT" + Math.floor(Math.random() * 100) + name1;
+        cy.get("textarea[name='newTask']").type(taskName);
+
+        const receiver = 'All Consensual';
+        cy.get("input[name='newReceiver']").type(receiver);
 
         cy.get("button.pull-right").should('have.attr', 'disabled', 'disabled');
         cy.get("div.tomorrow").should('contain', 'tomorrow');
